3622c9adb89a7418463e6f9fba7861989f9e96a0
Mohamed Hagras <mohamed.hagras2002@gmail.com>
2023-01-12 11:27:07 +0200

    changed link back to heroku hosted server

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
 const client = new ApolloClient({
-  uri: "https://online-store-api.onrender.com",
+  uri: "https://online-store-api-mohagras.herokuapp.com",
   cache: new InMemoryCache(),
 });
 
